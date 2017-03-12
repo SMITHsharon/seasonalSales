7af3f491624aace83c7c672193ab554a30794fb0
sharon smith <sharon.e.smith2@gmail.com>
2017-03-11 21:35:16 -0600

    xhr requests working correctly

--- sales.js
+++ sales.js
@@ -1,2 +1,43 @@
 
 console.log("writing to console");
+
+
+
+function executeProductsDOMAfterFileLoaded(){
+	var data = JSON.parse(this.responseText);
+	console.log("products data :: ", data);
+	// writeProductsDOM(data);
+}
+
+
+function executeCategoriesDOMAfterFileLoaded(){
+	var data = JSON.parse(this.responseText);
+	console.log("categories data :: ", data);
+	// writeCategoriesDOM(data);
+}
+
+function executeThisCodeAfterFileFails(){
+	console.log("whoops! error ... ");
+}
+
+
+
+//**************************************************
+// executes the XHR requests
+//**************************************************
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", executeProductsDOMAfterFileLoaded);
+myRequest.addEventListener("error", executeThisCodeAfterFileFails);
+myRequest.open("GET", "products.json");
+myRequest.send();
+
+var myRequest2 = new XMLHttpRequest();
+myRequest2.addEventListener("load", executeCategoriesDOMAfterFileLoaded);
+myRequest2.addEventListener("error", executeThisCodeAfterFileFails);
+myRequest2.open("GET", "categories.json");
+myRequest2.send();
+
+
+
+
+
